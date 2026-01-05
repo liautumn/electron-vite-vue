519edcfc19a16a12d401e48bc20a761e9c247b7e
liqiuzhuang <1060590974@qq.com>
2026-01-05 15:59:57 +0800

    提交代码

---
+++ src/types/serial.ts
@@ -0,0 +1,25 @@
+export interface SerialMethods {
+    /** 获取串口列表 */
+    list(): Promise<any[]>
+
+    /** 打开串口 */
+    open(options: { path: string; baudRate: number }): Promise<void>
+
+    /** 关闭串口 */
+    close(): Promise<void>
+
+    /** 发送十六进制数据 */
+    write(hex: string): Promise<void>
+
+    /** 串口打开事件 */
+    onOpen(cb: () => void): void
+
+    /** 串口关闭事件 */
+    onClose(cb: () => void): void
+
+    /** 串口数据事件 */
+    onData(cb: (event: Electron.IpcRendererEvent, data: string) => void): void
+
+    /** 串口错误事件 */
+    onError(cb: (event: Electron.IpcRendererEvent, msg: string) => void): void
+}

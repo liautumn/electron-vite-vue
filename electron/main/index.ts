578f6849cc882a20dd39df1d3a92d34a394e0501
liqiuzhuang <1060590974@qq.com>
2026-04-17 17:39:12 +0800

    优化主进程启动日志文案，移除多余结尾标记

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -59,7 +59,7 @@ process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
     ? path.join(process.env.APP_ROOT, 'public')
     : RENDERER_DIST
 
-log.info('=========>> Main process bootstrapped <<=========', {
+log.info('=========>> Main process bootstrapped', {
     isPackaged: app.isPackaged,
     logDirectory: getLogDirectory(),
     logFile: getLogFilePath(),
